3172f48c96d5e6e053bf304cff69de3ce50b08f2
sabrinapedroso <107559450+sabrinapedroso@users.noreply.github.com>
2022-12-07 08:45:47 -0300

    5.0

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 function Enviar(){
-    var nome = document.getElementById("nome")
-    var nome = document.getElementById("id")
-}
+    var nome= document.getElementById("nome")
+    var email= document.getElementById("email")
+    
+    if(nome.value="" && nome.value.indexOf("")!=-1){
+    if(email.value.indexOf("@")!=-1){
+        alert(nome.value+" Agradecemos o seu contato!")
+    }
+        else{
+        alert("Digite um email valido")
+        ok=false
+    }
+    }
+    else{
+    alert("Preencha o seu nome completo")
+    ok=false
+    }
+    }
